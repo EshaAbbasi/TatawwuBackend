142c0c620d6a0cd605a09082f2b5fb2164519513
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-12 18:57:31 +0300

    fix: added the router to the server and clean up server code formatting

--- routes/campaignRouter.js
+++ routes/campaignRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const campaignCtrl = require("../controllers/campaignCtrl");
 
 router.post("/", campaignCtrl.create);
-router.get("/index", campaignCtrl.index);
+router.get("/", campaignCtrl.index);
 router.get("/:id", campaignCtrl.show);
 router.put("/:id/update", campaignCtrl.update);
 router.delete("/:id/delete", campaignCtrl.delete);
