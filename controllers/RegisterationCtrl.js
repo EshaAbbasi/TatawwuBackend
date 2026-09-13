c07fc9022f11ad6888645fb3a648fe93fdccaf23
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-09-13 15:16:24 +0300

    Add Update

--- controllers/RegisterationCtrl.js
+++ controllers/RegisterationCtrl.js
@@ -42,11 +42,33 @@ const create = async (req, res) => {
 
 };
 
+
+
+
+    const update = async (req, res) => {
+try{
+const registration = await Registration.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!registration) {
+        return res.status(404).json({ error: "Registration not found" });
+    }
+    res.status(200).json(registration);
     
+
+
+}catch(error){
+            res.status(500).json({ error: error.message });
+
+}
+        
+};
+
+
+
 
 
 module.exports = {
     index,
     show,
     create,
+    update,
 };
