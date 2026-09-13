196c148c168770c976ae53c25bb603060a096f78
Esha Abbasi <eshaashfar@gmail.com>
2026-09-13 10:51:27 +0300

    added model for organization

---
+++ models/organization.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+ const organizationSchema=new mongoose.schema({
+ownerId:{
+type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique:true,
+},
+name:{
+  type:String,
+      required: true,
+    
+},
+description:{
+    type:String,
+    required:true,
+},
+country:{
+     type: String,
+      fixed: "BH",
+},
+governorate:{
+      type: String,
+     enum: ["Capital", "Northern", "Southern", "Muharraq", "Riffa"],
+      required: true,
+},
+area:{
+    type:String,
+    required:true,
+},
+address:{
+     type:String,
+    required:true,
+},
+contactEmail:{
+     type:String,
+   
+},
+contactPhone:{
+     type:String,
+   
+},
+whatsappNumber:{
+     type:String,
+   
+},
+website:{
+     type:String,
+   
+},
+logo:{
+     type:String,
+    
+},
+status:{
+     type:String,
+       enum: [
+        "Draft",
+        "Pending",
+        "Approved",
+        "Rejected",
+        "Removed",
+        "Completed",
+        "Cancelled",
+      ],
+    default:'pending',
+},
+
+
+  timestamps:{
+    required:true,
+  } 
+
+ })
+
+ const Organization=mongoose.model("Organization",organizationSchema);
+ module.exports=Organization;
