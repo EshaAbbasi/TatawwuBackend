c453dd43750d97b771b254065cd63c6ba2e48465
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-09-13 13:10:35 +0300

    create and finish RegisterationModel

---
+++ models/Registeration.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const registrationSchema = new mongoose.Schema(
+    {
+
+        volunteerId:{
+            type: mongoose.Schema.types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+        CampaignId:{
+            type: mongoose.Schema.types.ObjectId,
+            ref:"Campaign",
+            required:true,
+        },
+        status:{
+            type:String,
+            enum:["Registered","Cancelled"],
+            default:"Registered",
+        },
+        attendance:{
+            type:String,
+            enum:["Unmarked","Attended","Absent"],
+            default:"Unmarked",
+        },
+    },
+    {
+
+        timestamps:true,
+    }
+    
+);
+
+registrationSchema.index({ volunteerId: 1, CampaignId: 1 }, { unique: true });
+
+const Registration = mongoose.model('Registration', registrationSchema);
+module.exports = Registration;
